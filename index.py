f69d55fe574f4e970a963b6d5b71b0e3207262a1
sxcheng <1032211967@qq.com>
2018-12-14 17:07:35 +0800

    切换标签待完成

--- index.py
+++ index.py
@@ -17,8 +17,12 @@ tagHtmlSrc = ''
 extractCategory = re.compile(r'[\s\·\n]+')
 # 正则-提取标签
 extractTag = re.compile(r'[(\d)\s\n]+')
+# 正则-移除空格回车换行
+formatNull = re.compile(r'[\s\n\r]+')
 # tag_list
 tagList = []
+# 爬取结果， 仅存储单个标签的内容，防止内存占用过大
+results = {}
 # User-Agent列表，随机使用不同的浏览器头跳过反爬虫机制
 UserAgents = [
   'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
@@ -80,19 +84,87 @@ class MyTagHTMLParser(HTMLParser):
 # 解析书单
 class MyBookHTMLParser(HTMLParser):
 
+  # 解析标签
+
   def __init__(self, *, convert_charrefs=True):
     HTMLParser.__init__(self)
     self.cutItem = False
+    self.item = {
+      'name': ''
+    }
+    # 判断是否在书籍条目的范围内
+    self.is_item = False
+    self.itemList = []
+    # 解析内容
+    self.h2_text = False
+    self.pub_text = False
+    self.rat_text = False
+    self.pl_text = False
+    self.ir_text = False
   
   def handle_starttag(self, tag, attrs):
-    pass
-  
-  def handle_endtag(self, tag):
-    pass
-  
-  def handle_data(self, data):
-    pass
+    if tag == 'li' and len(attrs) > 0 and attrs[0][1] == 'subject-item':
+      self.is_item = True
 
+    if self.is_item:
+
+      if tag == 'img':
+        self.item['imgSrc'] = attrs[1][1]
+
+      if tag == 'h2':
+        self.h2_text = True
+
+      if len(attrs) > 0 and attrs[0][1] == 'pub':
+        self.pub_text = True
+
+      if len(attrs) > 0 and 'allstar' in attrs[0][1]:
+        self.item['allStar'] = attrs[0][1][:8]
+
+      if len(attrs) > 0 and attrs[0][1] == 'rating_nums':
+        self.rat_text = True
+
+      if len(attrs) > 0 and attrs[0][1] == 'pl':
+        self.pl_text = True
+
+      if tag == 'p':
+        self.ir_text = True
+      
+  def handle_endtag(self, tag):
+    if self.is_item:
+      if tag == 'h2':
+        self.h2_text = False
+      if tag == 'div' and self.pub_text == True:
+        self.pub_text = False
+      if tag == 'span':
+        self.rat_text = False
+        self.pl_text = False
+      if tag == 'p':
+        self.ir_text = False
+      if tag == 'li':
+        # 还原状态
+        self.is_item = False
+        self.itemList.append(self.item.copy()) 
+        self.item = {
+          'name': ''
+        }
+  def handle_data(self, data):
+    # 获取书名
+    if self.h2_text:
+      self.item['name'] = self.item['name'] + re.sub(formatNull,'',data)
+    # 获取书籍信息
+    if self.pub_text:
+      self.item['msg'] = re.sub(formatNull,'',data)
+    # 获取评分
+    if self.rat_text:
+      self.item['grade'] = re.sub(formatNull,'',data)
+    # 获取评论人数
+    if self.pl_text:
+      self.item['comment_num'] = re.findall('\d+',re.sub(formatNull,'',data))[0]
+    # 获取简介
+    if self.ir_text:
+      self.item['introduction'] = re.sub(formatNull,'',data)
+  
+  
 # 爬虫主体
 def bookSpider(category, tag):
   # 页码
@@ -104,13 +176,13 @@ def bookSpider(category, tag):
     parserBook.feed(getBook(tag, num))
 
     parserBook.close()
-    print(getBook(tag,num))
+    
     #延时
     time.sleep(random() * 5)
     # 是否切换条例
     if parserBook.cutItem:
       num = num + 20
-      crawing()
+      crawing(num)
     else:
       return
   
