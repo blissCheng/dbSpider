7dc7f674fccdfcf9b354db443c47a3fd9910bac8
sxcheng <1032211967@qq.com>
2018-12-17 17:09:40 +0800

    end

--- index.py
+++ index.py
@@ -5,6 +5,9 @@ import os
 import re
 import pickle
 import time
+import json
+import urllib
+import socket
 from urllib import parse
 from urllib import request
 #from config import config
@@ -22,13 +25,14 @@ formatNull = re.compile(r'[\s\n\r]+')
 # tag_list
 tagList = []
 # 爬取结果， 仅存储单个标签的内容，防止内存占用过大
-results = {}
+results = []
 # User-Agent列表，随机使用不同的浏览器头跳过反爬虫机制
 UserAgents = [
   'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
   'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
   'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'
 ]
+
 # 请求分类接口
 with request.urlopen('https://book.douban.com/tag/?view=type&icn=index-sorttags-all') as dbTag:
   data = dbTag.read()
@@ -40,9 +44,15 @@ def getBook(tag, start):
   req = request.Request('https://book.douban.com/tag/%s?start=%s&type=T' % (parse.quote(tag), str(start)))
   # 添加请求头
   req.add_header('User-Agent', UserAgents[start % len(UserAgents)])
+  try:
+    with request.urlopen(req, timeout=60) as bookList:
+      data = bookList.read().decode('utf-8')
 
-  with request.urlopen(req) as bookList:
-    data = bookList.read().decode('utf-8')
+  except:
+    print('查询接口异常: 重新开始请求标签 %s start %s' % (tag, start))
+    data = getBook(tag, start)
+      
+
   return data
 
 # 解析tag页
@@ -103,6 +113,7 @@ class MyBookHTMLParser(HTMLParser):
     self.ir_text = False
   
   def handle_starttag(self, tag, attrs):
+
     if tag == 'li' and len(attrs) > 0 and attrs[0][1] == 'subject-item':
       self.is_item = True
 
@@ -148,6 +159,9 @@ class MyBookHTMLParser(HTMLParser):
           'name': ''
         }
   def handle_data(self, data):
+    if re.sub(formatNull, '', data) == '没有找到符合条件的图书':
+      print('开始切换标签')
+      self.cutItem = True
     # 获取书名
     if self.h2_text:
       self.item['name'] = self.item['name'] + re.sub(formatNull,'',data)
@@ -159,7 +173,9 @@ class MyBookHTMLParser(HTMLParser):
       self.item['grade'] = re.sub(formatNull,'',data)
     # 获取评论人数
     if self.pl_text:
-      self.item['comment_num'] = re.findall('\d+',re.sub(formatNull,'',data))[0]
+      matchList = re.findall('\d+',re.sub(formatNull,'',data))
+      # 筛选评论人数
+      self.item['comment_num'] = matchList[0] if (isinstance(matchList, list) and len(matchList) > 0) else 0
     # 获取简介
     if self.ir_text:
       self.item['introduction'] = re.sub(formatNull,'',data)
@@ -171,30 +187,54 @@ def bookSpider(category, tag):
   pageNum = 0
 
   def crawing(num):
+    # 开始解析
     parserBook = MyBookHTMLParser()
 
     parserBook.feed(getBook(tag, num))
 
     parserBook.close()
-    
+    # 合并进结果集
+    results.extend(parserBook.itemList)
+
+    print('%s-%s  start-%s 结束' % (category, tag, num))
     #延时
-    time.sleep(random() * 5)
+    time.sleep(int(random() * 5))
     # 是否切换条例
-    if parserBook.cutItem:
+    if not parserBook.cutItem:
+      print('开始查询下一页')
       num = num + 20
       crawing(num)
     else:
+      # 结果集写入文件
+      writeToJson(category, tag, json.dumps(results, ensure_ascii=False, indent=2))
       return
   
   crawing(pageNum)
   return
 
+def writeToJson(category, tag, data):
+  with open(os.path.join(os.path.abspath('.'), category + '/' + tag + '.json'), 'a', encoding = 'utf-8') as jsonFile:
+    jsonFile.write(data)
 
 # 开始爬取
 def startSpider():
+  # 开始位置
+  if len(sys.argv) > 1:
+    ARGTAG = sys.argv[1]
+  else:
+    ARGTAG = False
   for x in tagList:
     for n in tagList[x]:
-      bookSpider(x, n)
+      # 每一个新标签开始时清空结果集
+      # 命名为全局变量，虽然已声明全局变量results，但是如果在这里初始化results, python会认为你创建了一个局部变量
+      global results
+      results = []
+      if ARGTAG and ARGTAG == n:
+        # 如果从某一节点开始爬取
+        bookSpider(x, n)
+        ARGTAG = False
+      elif not ARGTAG:
+        bookSpider(x, n)
 
 if __name__ == '__main__':
 
